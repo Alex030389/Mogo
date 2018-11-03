83d526e223aec254d0f62ee933fecfaf577c0ed1
AlexKa <x01z@mail.ru>
2018-11-03 12:52:44 +0300

    map

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,13 @@
 "use strict";
-// libs
-//= ../libs/jquery.min.js
+// = ../libs/jquery.min.js
 
-$(function() {
-
-	var hamburger = $(".hamburger");
-	var headerNav = $(".header__nav")
-  hamburger.on("click", function(e) {
-		hamburger.toggleClass("is-active");
+$(function () {
+  
+  var hamburger = $(".hamburger");
+  var headerNav = $(".header__nav")
+  hamburger.on("click", function (e) {
+    hamburger.toggleClass("is-active");
     headerNav.toggleClass("header__nav_active");
   });
-
+  
 });
