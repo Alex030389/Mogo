56bad3e85b8fb5acc4408a635e821a667b602e5c
AlexKa <x182838@gmail.com>
2019-02-09 10:22:20 +0300

    new gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,161 +1,76 @@
-// 'use strict';
+var syntax        = 'scss'; // Syntax: sass or scss;
 
-var gulp        = require('gulp'),
-    watch       = require('gulp-watch'),
-    prefixer    = require('gulp-autoprefixer'),
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    browserSync   = require('browser-sync'),
     concat        = require('gulp-concat'),
-    uglify      = require('gulp-uglify'),
-    sass        = require('gulp-sass'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    cssmin      = require('gulp-clean-css'),
-    plumber     = require('gulp-plumber'),
-	notify      = require('gulp-notify'),
-    del         = require('del'),
-    browserSync = require("browser-sync"),
-    reload      = browserSync.reload;
+    uglify        = require('gulp-uglify'),
+    cleancss      = require('gulp-clean-css'),
+    rename        = require('gulp-rename'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    notify        = require('gulp-notify'),
+    del           = require('del'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    babel         = require("gulp-babel");
 
-var path = {
-    build: {
-        html: 'dist/',
-        js: 'dist/js/',
-        css: 'dist/css/',
-        img: 'dist/img/',
-        imgAll: 'dist/img/all',
-        fonts: 'dist/fonts/'
-    },
-    src: {
-        html: 'src/*.html',
-        js: [
-            './node_modules/jquery/dist/jquery.min.js',
-            "./node_modules/slick-carousel/slick/slick.min.js",
-            './src/js/main.js'
-        ],
-        style: 'src/style/main.scss',
-        img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
-    },
-    watch: {
-        html: 'src/**/*.html',
-        js: 'src/js/**/*.js',
-        style: 'src/style/**/*.scss',
-        img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
-    },
-    clean: 'dist'
-};
-
-var config = {
+gulp.task('browser-sync', function() {
+  browserSync({
     server: {
-        baseDir: 'dist'
+      baseDir: 'dist'
     },
-    tunnel: true,
-    host: 'localhost',
-    port: 9000,
-    logPrefix: "Ok"
-};
-
-gulp.task('webserver', function () {
-    browserSync(config);
+    notify: false,
+    // open: false,
+    // online: false, // Work Offline Without Internet Connection
+    // tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+  })
 });
 
 gulp.task('clean', function () {
-    del(path.clean);
+  return del(['dist/css', 'dist/js']);
 });
 
-gulp.task('html:build', function () {
-    gulp.src(path.src.html)
-        .pipe(gulp.dest(path.build.html))
-        .pipe(reload({stream: true}));
+gulp.task('html', function() {
+  return gulp.src('dist/*.html')
+  .pipe(browserSync.stream())
 });
 
-gulp.task('js:build', function () {
-    gulp.src(path.src.js)
-        .pipe(sourcemaps.init())
-        .pipe(concat('main.min.js'))
-        .pipe(uglify()) 
-        .pipe(sourcemaps.write()) 
-        .pipe(gulp.dest(path.build.js))
-        .pipe(reload({stream: true}));
+gulp.task('styles', function() {
+  return gulp.src('src/'+syntax+'/**/*.'+syntax+'')
+  .pipe(sourcemaps.init())
+  .pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+  .pipe(rename({ suffix: '.min', prefix : '' }))
+  .pipe(autoprefixer(['last 15 versions']))
+  .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('dist/css'))
+  .pipe(browserSync.stream())
 });
 
-gulp.task('style:build', function () {
-    gulp.src(path.src.style) 
-        .pipe(plumber({
-            errorHandler: notify.onError(function(err){
-                return {
-                    title: 'Styles',
-                    message: err.message
-                }
-            })
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            sourceMap: true,
-            errLogToConsole: true
-        }))
-        .pipe(prefixer())
-        .pipe(cssmin())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.build.css))
-        .pipe(reload({stream: true}));
+gulp.task('scripts', function() {
+  return gulp.src([
+    'src/js/common.js'
+    ])
+  .pipe(sourcemaps.init())
+  .pipe(concat('scripts.min.js'))
+  .pipe(babel({
+    presets: ['@babel/env']
+  }))
+  // .pipe(uglify()) // Mifify js (opt.)
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('dist/js'))
+  .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('image:build', function () {
-    gulp.src(path.src.img) 
-        .pipe(gulp.dest(path.build.img))
-        .pipe(reload({stream: true}));
+gulp.task('watch', function() {
+  gulp.watch('dist/*.html', gulp.parallel('html'));
+  gulp.watch('src/'+syntax+'/**/*.'+syntax+'', gulp.parallel('styles'));
+  // gulp.watch(['libs/**/*.js', 'src/js/**/*'], gulp.parallel('scripts'));
+  gulp.watch('src/js/**/*', gulp.parallel('scripts'));
 });
 
-gulp.task('fonts:build', function() {
-    gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts))
-});
-
-gulp.task('build', [
-    'html:build',
-    'js:build',
-    'style:build',
-    'fonts:build',
-    'image:build'
-]);
-
-
-gulp.task('watch', function(){
-    watch([path.watch.html], function(event, cb) {
-        gulp.start('html:build');
-    });
-    watch([path.watch.style], function(event, cb) {
-        gulp.start('style:build');
-    });
-    watch([path.watch.js], function(event, cb) {
-        gulp.start('js:build');
-    });
-    watch([path.watch.img], function(event, cb) {
-        gulp.start('image:build');
-    });
-    watch([path.watch.fonts], function(event, cb) {
-        gulp.start('fonts:build');
-    });
-});
-
-
-gulp.task('default', ['build', 'webserver', 'watch']);
-
-// browser-sync
-// gulp
-// gulp-autoprefixer
-// gulp-clean-css
-// gulp-concat
-// gulp-sass
-// gulp-sourcemaps
-// gulp-uglify
-// gulp-watch
-// del
-// gulp-notify(выводит ошибки)
-// gulp-plumber
-
-// ===============================
-// gulp-concat(заменили на gulp-rigger)
-// gulp-rename(добавляет .min)
-// css-comb - уснановить вместо расширения
-// из папки img при билде исключить папку all!!!
+gulp.task('default', gulp.parallel(
+  'clean',
+  'styles',
+  'scripts',
+  'browser-sync',
+  'watch'));
